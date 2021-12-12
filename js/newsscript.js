e2ca5687392712e6d756fa3cda66f02422835ee5
relaxeddddd <75780693+relaxeddddd@users.noreply.github.com>
2021-12-12 23:30:28 +0300

    Add files via upload

---
+++ js/newsscript.js
@@ -0,0 +1,29 @@
+$(function () {
+	$('.chart').easyPieChart({
+		lineWidth: 2,
+		scaleColor: false,
+		size: 60,
+		barColor: '#b50051',
+		trackColor: '#e3e3e3'
+	});
+});
+
+$(function () {
+	$('.chart2').easyPieChart({
+		lineWidth: 2,
+		scaleColor: false,
+		size: 60,
+		barColor: '#055454',
+		trackColor: '#e3e3e3'
+	});
+});
+
+$(function () {
+	$('.chart3').easyPieChart({
+		lineWidth: 2,
+		scaleColor: false,
+		size: 60,
+		barColor: '#8bd0c6',
+		trackColor: '#e3e3e3'
+	});
+})
